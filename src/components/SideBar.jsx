99ddc1dbd02abb085da258e12c59f223fc1caa3e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-05-29 15:27:52 -0300

    feat: sidebar done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import styles from './SideBar.module.css';
+import { PencilSimple } from 'phosphor-react';
 
 export function Sidebar() {
   return (
@@ -7,13 +8,23 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1601645191163-3fc0d5d64e35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
       />
+
       <div className={styles.profile}>
+        <img
+          src="https://lh3.googleusercontent.com/a/AAcHTteo5eEKMqEfRCJdsJZUm5stTMYPh9dWUni2Rc18=s83-c-mo"
+          alt="foto de perfil usuário"
+          className={styles.avatar}
+        />
+
         <strong>Marcos Gabriel</strong>
         <span>Software Engineer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilSimple size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
