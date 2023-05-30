1991a08ba1c72fe59063ff3afaa165ce8619f3ed
Marc0sGabriel <mgabriel4747@gmail.com>
2023-05-30 15:06:52 -0300

    feat: comments and styles

---
+++ src/components/Comment.jsx
@@ -0,0 +1,42 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://lh3.googleusercontent.com/a/AAcHTteo5eEKMqEfRCJdsJZUm5stTMYPh9dWUni2Rc18=s83-c-mo"
+        alt="foto de perfil usuário"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcos Gabriel</strong>
+              <time
+                title="29 de maio às 16:04:00"
+                dateTime="2023-05-29 16:04:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} weight="bold" />
+            </button>
+          </header>
+
+          <p>Nossa que projeto incrivel!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={16} weight="regular" />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
