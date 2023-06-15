d2a0ac8222005b0b3970b88248eb4c63b94746eb
Marc0sGabriel <mgabriel4747@gmail.com>
2023-06-15 15:45:35 -0300

    feat: add button to post

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,30 +1,28 @@
+import { PaperPlane } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './NewPost.module.css';
-
-interface Author {
-  avatarUrl: string;
-}
-
-interface NewPostProps {
-  author: Author;
-}
+import { useState } from 'react';
 
 export function NewPost() {
+  const [newPostText, setNewPostText] = useState('');
+  const isEmpty = newPostText.length === 0;
+
   return (
     <>
       <div className={styles.createNewPostContainer}>
-        <header>
-          <Avatar
-            src="https://github.com/Marc0sGabriel.png"
-            alt={'foto de perfil'}
-          />
-          <form>
-            <textarea
-              name="createPost"
-              placeholder="Quais as novidades?"
-            ></textarea>
-          </form>
-        </header>
+        <Avatar
+          src="https://github.com/Marc0sGabriel.png"
+          alt={'foto de perfil'}
+        />
+        <form className={styles.newPostForm}>
+          <header>
+            <textarea name="createPost" placeholder="Quais as novidades?" />
+
+            <button type="submit" disabled={isEmpty}>
+              <PaperPlane size={23} weight="bold" />
+            </button>
+          </header>
+        </form>
       </div>
     </>
   );
