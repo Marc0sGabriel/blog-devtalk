ef443f1b01873b203966f03a4e8e03e46ecf69cd
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-31 19:08:29 -0300

    :lipstick: feat: default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,27 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#eef0f1',
+  'gray-200': '#d2d5d9',
+  'gray-300': '#c4c4cc;',
+  'gray-500': '#7b7b8b',
+  'gray-900': '#08090a',
+
+  'green-100': '#e9fbfa',
+  'green-300': '#00B37E',
+  'green-400': '#19b2a6',
+  'green-500': '#00875f',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+  'red-400': '#f75a68',
+
+  'blue-100': '#ebedfc',
+  'blue-600': '#3b49df',
+
+  ' purple-500': ' rgb(149, 51, 206)',
+  ' purple-700': ' rgb(140, 43, 197)',
+
+  'yellow-100': '#fffaed',
+  'yellow-600': '#f5b400',
+};
