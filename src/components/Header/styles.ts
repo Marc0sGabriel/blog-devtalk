bb719bbfc4f5818e521047baefb5fe2e3d7426d8
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-31 19:06:50 -0300

    :lipstick: feat: header styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import { styled } from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  & {
+    position: sticky;
+    top: 0;
+    z-index: 60;
+    backdrop-filter: blur(10px);
+    margin-bottom: 3rem;
+    background: ${(props) => props.theme['white']};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    padding: 1.25rem 0;
+  }
+
+  & strong {
+    font-size: 1.25rem;
+  }
+
+  .logoContainer {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
