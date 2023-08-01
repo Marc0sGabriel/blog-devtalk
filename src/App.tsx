c14e633fffbb6e01588462f36c2484c8a5637f9e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-01 15:45:47 -0300

    :package: feat: update import NewPost

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/SideBar';
 import { ThemeProvider } from 'styled-components';
 import styles from './App.module.css';
-import { NewPost } from './components/NewPost';
+import { NewPost } from './components/NewPost/NewPost';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 
