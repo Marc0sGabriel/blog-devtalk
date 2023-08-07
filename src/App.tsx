66d5e52bccc6070d0affe333a7944fc3d0cc6171
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-07 16:27:19 -0300

    :sparkles: feat: add RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Router } from './routes/Router';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { router } from './routes/Router';
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
