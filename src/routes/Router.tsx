7d0f48624b5956b11bcc59df27609d7ec0634227
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-07 16:33:03 -0300

    :recycle: change: routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/Home/Home';
 import { Page404 } from './PageNotFound';
+import { Default } from '../layouts/DefaultLayout';
 
-export function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Page404 />} />
-    </Routes>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Default />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+
+  {
+    path: '*',
+    element: <Page404 />,
+  },
+]);
