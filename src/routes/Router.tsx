5ca8edb2ad6aec102d4726a8aaabe096603e0af1
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-10 14:59:20 -0300

    :sparkles: feat: New Routes, Saved, Explore

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,22 +1,34 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Home } from '../pages/Home/Home';
-import { Page404 } from './PageNotFound';
-import { Default } from '../layouts/DefaultLayout';
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../pages/Home/Home";
+import { Page404 } from "./PageNotFound";
+import { Default } from "../layouts/DefaultLayout";
+import { Saved } from "../pages/Saved";
+import { Explore } from "../pages/Explorar";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Default />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
+      },
+
+      {
+        path: "/saved",
+        element: <Saved />,
+      },
+
+      {
+        path: "/explore",
+        element: <Explore />,
       },
     ],
   },
 
   {
-    path: '*',
+    path: "*",
     element: <Page404 />,
   },
 ]);
