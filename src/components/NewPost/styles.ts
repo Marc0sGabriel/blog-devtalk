4c9c866ae6fbdbeedee5f89191cfa96ecf4c1c02
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-10 14:53:42 -0300

    :lipstick: feat: Modal Styles

--- src/components/NewPost/styles.ts
+++ src/components/NewPost/styles.ts
@@ -1,11 +1,17 @@
-import { styled } from 'styled-components';
+import {
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+} from "@radix-ui/react-dialog";
+import { styled } from "styled-components";
 
 export const NewPostContainer = styled.div`
   position: relative;
   top: 0.5rem;
-  z-index: 50;
 
-  background-color: ${(props) => props.theme['white']};
+  background-color: ${(props) => props.theme["white"]};
   backdrop-filter: blur(10px);
   border-radius: 8px;
   padding: 1rem 1rem;
@@ -25,24 +31,24 @@ export const NewPostContainer = styled.div`
     padding-bottom: 1rem;
 
     textarea {
-      width: 80%;
+      width: 100%;
 
       resize: none;
       overflow: hidden;
       overflow: -moz-hidden-unscrollable;
-      background: ${(props) => props.theme['gray-100']};
+      background: ${(props) => props.theme["gray-100"]};
       padding: 0.875rem;
       height: 3rem;
       border: 0;
       line-height: 1.4;
-      color: ${(props) => props.theme['gray-900']};
+      color: ${(props) => props.theme["gray-900"]};
       margin-top: 1rem;
       border-radius: 8px;
     }
 
     textarea::placeholder {
       font-weight: 500;
-      color: ${(props) => props.theme['gray-500']};
+      color: ${(props) => props.theme["gray-500"]};
     }
   }
 
@@ -56,35 +62,106 @@ export const NewPostContainer = styled.div`
       display: flex;
       justify-content: space-around;
       align-items: center;
+    }
+  }
+`;
 
-      button {
-        visibility: hidden;
-        max-height: 0;
-      }
+export const Title = styled(DialogTitle)`
+  color: ${(props) => props.theme["green-400"]};
+`;
 
-      button {
-        margin-top: 1rem;
-        background: ${(props) => props.theme['green-400']};
-        border: none;
-        border-radius: 9999px;
-        height: 3rem;
-        width: 3rem;
-        color: ${(props) => props.theme['gray-100']};
+export const ModalOverlay = styled(DialogOverlay)`
+  background: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  inset: 0;
+  z-index: 60;
+  backdrop-filter: blur(10px);
+`;
 
-        font-weight: bold;
+export const ModalContent = styled(DialogContent)`
+  background-color: white;
+  border-radius: 8px;
+  position: fixed;
+  z-index: 70;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 60rem;
+  max-height: 85vh;
+  padding: 25px;
 
-        cursor: pointer;
-      }
+  textarea {
+    width: 100%;
+    height: 100%;
+    resize: none;
+    border: 1px solid ${(props) => props.theme["gray-100"]};
+    border-radius: 4px;
+    padding: 1rem;
+  }
 
-      button[type='submit']:not(:disabled):hover {
-        background: ${(props) => props.theme['green-300']};
-        transition: 0.3s all;
-      }
+  button:first-of-type {
+    margin-right: 1rem;
+  }
 
-      button:disabled {
-        cursor: not-allowed;
-        opacity: 0.5;
-      }
+  .Editor {
+    margin: 1rem 0;
+    width: 100%;
+    height: 20rem;
+  }
+
+  .xIcon {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    outline: none;
+
+    &:hover {
+      filter: opacity(0.4);
+    }
+  }
+
+  .headerModal {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .PublishButton {
+    padding: 0.875rem 1.25rem;
+    font-weight: bold;
+    background-color: ${(props) => props.theme["purple-500"]};
+    color: ${(props) => props.theme["white"]};
+    border: none;
+    outline: none;
+    border-radius: 50px;
+    cursor: pointer;
+
+    &[type="submit"]:not(:disabled):hover {
+      border: 0;
+      filter: brightness(0.9);
+      transition: all 0.3s;
+      cursor: pointer;
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.5;
+    }
+  }
+
+  .Cancel {
+    padding: 0.875rem 1.25rem;
+    font-weight: bold;
+    background-color: transparent;
+    border: 1px solid ${(props) => props.theme["red-400"]};
+    color: ${(props) => props.theme["red-400"]};
+    border-radius: 50px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background: ${(props) => props.theme["red-400"]};
+      color: white;
     }
   }
 `;
